848a45df8052b10886eb6c4e62ddb78f47bb1372
Alex Covington <alex@covington.tech>
2022-02-01 22:52:48 -0800

    Better rendering of posts

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -2,12 +2,13 @@
   <h1>{{ $route.params.title }}</h1>
   <div v-if="loadingPost">Loading...</div>
   <div v-else-if="postBody != null">
-      <vue-markdown>{{ postBody }}</vue-markdown>
+      <div v-html="postBody"></div>
   </div>
   <div v-else>Something went wrong...</div>
 </template>
 
 <script>
+import { marked } from 'marked';
 const api = 'https://cdn.covington.tech/blog/posts';
 
 export default {
@@ -23,7 +24,7 @@ export default {
       this.axios.get(`${api}/${this.$route.params.title}.md`)
         .then((res) => {
             console.log(res);
-            this.postBody = res.data;
+            this.postBody = marked(res.data, { sanitize: true });
         })
         .catch((err) => {
             console.error(err);
